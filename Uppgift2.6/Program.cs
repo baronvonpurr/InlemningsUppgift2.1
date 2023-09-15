74113aee2e1e446a02e1e608551a7a575c96b32f
baronvonpurr <robinhood.svensson@hotmail.com>
2023-09-15 12:56:45 +0200

    Add project files.

---
+++ Uppgift2.6/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace InlämningsUppgift2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vilken är din favorit bok?");
+            string favvisbok = Console.ReadLine();
+            Console.WriteLine(favvisbok);
+            Console.ReadKey();
+        }
+    }
+}
